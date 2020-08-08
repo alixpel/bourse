726e20218a1ff4014491787105d17ca235d125f1
Alix Pelletier <alixpelletierpro@gmail.com>
2020-08-08 12:25:49 +0200

    Création du chart

--- stock_table.js
+++ stock_table.js
@@ -1,10 +1,27 @@
 'use strict';
-
+const {
+  LineChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Line,
+  Legend
+} = Recharts;
 class StockTable extends React.Component {
   render() {
     return (
       <div>
         <h2>25 premières valeurs</h2>
+
+        <LineChart width={500} height={300} data={this.props.stock_data}>
+          <XAxis dataKey="name"/>
+          <YAxis/>
+          <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+          <Line type="monotone" dataKey="stocks.CAC40" stroke="#8884d8" />
+          <Line type="monotone" dataKey="stocks.NASDAQ" stroke="#82ca9d" />
+          <Legend/>
+        </LineChart>
+
         <table>
           <tbody>
             <tr>
